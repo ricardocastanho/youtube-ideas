1426bc84f3056928f633264736c74453ec72bdf8
ricardocastanho <48383095+ricardocastanho@users.noreply.github.com>
2025-01-13 12:29:50 -0300

    fix: update extractYouTubeUrl

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,6 @@
 export const extractYouTubeURL = (input: string): string | null => {
   const youtubeRegex =
-    /(https?:\/\/(?:www\.)?youtube\.com\/(?:watch\?v=|embed\/|v\/|e\/|.*\/videoseries\?v=)([a-zA-Z0-9_-]{11}))/i;
+    /(https?:\/\/(?:www\.)?youtube\.com\/(?:watch\?v=|embed\/|v\/|e\/|.*\/videoseries\?v=|.*\?.*v=)|https?:\/\/youtu\.be\/)([a-zA-Z0-9_-]{11})/i;
   const match = input.match(youtubeRegex);
   return match ? match[0] : null;
 };
