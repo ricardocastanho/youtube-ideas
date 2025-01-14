9ad1c843a125526939634dddf50ca28c7b771411
ricardocastanho <48383095+ricardocastanho@users.noreply.github.com>
2025-01-14 17:49:56 -0300

    style: improve user message radius

--- src/components/UserMessage.tsx
+++ src/components/UserMessage.tsx
@@ -20,7 +20,12 @@ const UserMessage: React.FC<UserMessageProps> = ({ content }) => {
 
   return (
     <div>
-      <div className="bg-secondary rounded-full p-4 px-12">
+      <div
+        className={`bg-secondary p-4 px-12 ${
+          message.split("\n").filter(Boolean).length > 1 || message.length > 30
+            ? "rouded-3xl"
+            : "rounded-full"
+        }`}>
         {message.split("\n").map((word, i) => (
           <p className="text-white" key={i}>
             {word}
